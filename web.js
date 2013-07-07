9298ee6ca9541c18a5a112799ced72767ba7a032
Tiago Marques <tiago.m@laposte.net>
2013-07-07 14:18:06 +0000

    de novo

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var str = fs.readFileSync('index.html', 'utf8');
+//var str = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
   response.send('a');
